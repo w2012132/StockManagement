cfff247760f70de3b70904a9e44e7d2a1f7eba07
Al Masum <w2012132@westminster.ac.uk>
2024-03-05 10:16:03 +0000

    published profile added

--- StockManagementAPI/obj/Release/net8.0/StockManagementAPI.AssemblyInfo.cs
+++ StockManagementAPI/obj/Release/net8.0/StockManagementAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StockManagementAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+406ea0be0f0531396e234c9d42128d0ae8e03c16")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+06dec7d44e5ae5c9dff2b7a733b68e814d6be1f5")]
 [assembly: System.Reflection.AssemblyProductAttribute("StockManagementAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StockManagementAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
